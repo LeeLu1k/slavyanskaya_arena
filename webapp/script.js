aa6eda6da4c9f5e7e90ff387ccfcde99d8a7e463
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-06 20:23:30 +0500

    Update script.js

--- webapp/script.js
+++ webapp/script.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById("sendDataBtn");
 
 btn.addEventListener("click", () => {
-    // Отправляем данные боту
     if (window.Telegram.WebApp) {
         Telegram.WebApp.sendData("Привет, бот! Это данные из Web App.");
         alert("Данные отправлены боту!");
+    } else {
+        alert("Telegram Web App не доступен!");
     }
 });
