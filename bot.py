318685abbdb8350ef71743bc2dfede7770c1c6c9
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-06 20:23:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,29 @@
 import os
+import threading
 from telegram import Update, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
+WEBAPP_URL = "https://slavyanskayaarena-production.up.railway.app/webapp/index.html"
 
-WEBAPP_URL = "https://slavyanskayaarena-production.up.railway.app/webapp/index.html"  # URL твоего Web App
-
+# Запуск бота
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("Открыть мини-приложение", web_app=WebAppInfo(url=WEBAPP_URL))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Привет! Нажми кнопку ниже, чтобы открыть мини-приложение:", reply_markup=reply_markup)
+    await update.message.reply_text(
+        "Привет! Нажми кнопку ниже, чтобы открыть мини-приложение:", 
+        reply_markup=reply_markup
+    )
 
-if __name__ == "__main__":
+def run_bot():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     print("Бот запущен...")
     app.run_polling()
+
+# Запуск в отдельном потоке (чтобы не блокировал Flask)
+threading.Thread(target=run_bot).start()
